8f49fc9df70406c8ad66842864b36e673284bc8a
farmerintech <farmerintech@gmail.com>
2026-01-07 08:15:53 +0100

    added teachers reg

--- src/app/(navigation)/teachers/register/page.tsx
+++ src/app/(navigation)/teachers/register/page.tsx
@@ -1,24 +1,18 @@
 "use client";
 
-import { SignUpForm } from "@/app/authPage/signup/signupForm";
-import { X } from "lucide-react";
-
-interface SignUpPageProps {
-  setShowReg: (show: boolean) => void;
-}
-
-
+import { SignUpForm } from "./SignUpForm";
 
 export default function SignUpPage() {
   return (
-     <section className="flex items-center justify-center w-full h-screen bg-[#FFEEE6]">
-          <div className="w-full md:w-2/4 flex flex-col justify-center p-6 md:p-16 gap-3">
-            <h3 className="text-xl font-bold text-left">Sign Up</h3>
-            <p className="text-gray-500 text-xs text-left mb-3">
-              Welcome back! Please sign in to your account.
-            </p>
-            <SignUpForm />
-          </div>
-        </section>
+    <section className="flex items-center justify-center w-full">
+      <div className="w-full md:w-2/3 flex flex-col justify-center p-6 md:p-16 gap-3">
+        <h3 className="text-xl font-bold text-left">Sign Up For Teachers</h3>
+        <p className="text-gray-500 text-xs text-left mb-3">
+          Complete the form to register. Your account will be verified by admin.
+        </p>
+
+        <SignUpForm />
+      </div>
+    </section>
   );
 }
