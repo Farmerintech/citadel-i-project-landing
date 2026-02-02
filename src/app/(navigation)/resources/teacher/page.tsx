8569747eefa4e1d01bd3289f40f15c60d07e4be2
farmerintech <farmerintech@gmail.com>
2026-02-02 19:18:22 +0100

    corrected UI

--- src/app/(navigation)/resources/teacher/page.tsx
+++ src/app/(navigation)/resources/teacher/page.tsx
@@ -93,7 +93,8 @@ export default function ResourcesPage() {
           <div key={index} className="bg-white shadow px-3 rounded-xl">
             <div className="flex items-center justify-center w-full h-[200px] mb-2">
               {ResourcesImages.map(image => image.name === resource.source ?
-                <Image key={image.name} src={resource.filePath} alt={image.name} className='rounded-xl'/> : null
+                <Image key={image.name} src={resource.filePath ?? image.image} alt={image.name} width={100} height={100}
+                 className='rounded-xl w-full h-full'/> : null
               )}
             </div>
             <div className="flex gap-[16px] flex-col">
